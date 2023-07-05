7455e2670c809f343149f85d800761d04af1dea8
VsPot07 <cocoacake84@gmail.com>
2023-07-05 10:11:24 +0900

    0705

--- Assets/01_Scripts/Enemy_FSM/RobotEnemy/EnemyGrenadeAttack.cs
+++ Assets/01_Scripts/Enemy_FSM/RobotEnemy/EnemyGrenadeAttack.cs
@@ -6,12 +6,53 @@ using DG.Tweening;
 public class EnemyGrenadeAttack : EnemyAttack
 {
     [SerializeField] private GameObject _fireEffect;
+    [SerializeField] private float _Edamage;
+    LineRenderer[] _line = new LineRenderer[3];
+
+    private void Start()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            _line[i] = transform.Find($"LR_{i}").GetComponent<LineRenderer>();
+        }
+    }
+
     public override void AttackEvent(GameObject firePos)
     {
         GameObject effect = Instantiate(_fireEffect);
         effect.transform.position = firePos.transform.position;
         Vector3 dir = -thisParentEnemy.transform.forward;
+        AttackLogic(firePos);
         thisParentEnemy.transform.DOMove(new Vector3(dir.x, thisParentEnemy.transform.position.y, dir.z), 0.3f);
-        Debug.Log(new Vector3(dir.x, thisParentEnemy.transform.position.y, dir.z));
+    }
+
+    private void AttackLogic(GameObject firePos)
+    {
+        RaycastHit hit;
+        for (int i = 0; i < 3; i++)
+        {
+            Physics.Raycast(firePos.transform.position, transform.forward * (i - 1) * 30, out hit, 10);
+            Debug.Log(_line[i]);
+            _line[i].enabled = true;
+            _line[i].SetPosition(0, firePos.transform.position);
+            _line[i].SetPosition(1, hit.point);
+            StartCoroutine(TrailFade(i));
+            if (hit.collider.gameObject.TryGetComponent<HealthFunc>(out HealthFunc hf))
+            {
+                hf.DamageCalcculate(_Edamage);
+            }
+        }
+        IEnumerator TrailFade(int count)
+        {
+            Material _mat = _line[count].material;
+            float fadeCount = 0.5f;
+            while (fadeCount > 0)
+            {
+                _mat.color = new Color(1, 0.9f, 0.7f, fadeCount);
+                fadeCount -= 0.005f;
+                yield return null;
+            }
+        }
+
     }
 }
--- Assets/01_Scripts/Enemy_FSM/RobotEnemy/EnemyShootAttack.cs
+++ Assets/01_Scripts/Enemy_FSM/RobotEnemy/EnemyShootAttack.cs
@@ -4,34 +4,54 @@ using UnityEngine;
 
 public class EnemyShootAttack : EnemyAttack
 {
-    LineRenderer _line;
+    LineRenderer[] _line = new LineRenderer[3];
     [SerializeField] private GameObject _fireEffect;
     [SerializeField] private float _Edamage;
 
+    int count;
+
     private void Start()
     {
-        _line = GetComponent<LineRenderer>();
-        _line.enabled = false;
+        for(int i = 0; i < 3; i++)
+        {
+            _line[i] = transform.Find($"LR_{i}").GetComponent<LineRenderer>();
+            _line[i].enabled = false;
+        }
     }
 
     public override void AttackEvent(GameObject firePos)
     {
+        GameObject effect = Instantiate(_fireEffect);
+        effect.transform.position = firePos.transform.position;
         RaycastHit hit;
         if (Physics.Raycast(firePos.transform.position, transform.forward, out hit, 10))
         {
-            _line.enabled = true;
-            _line.SetPosition(0, firePos.transform.position);
-            _line.SetPosition(1, hit.point);
-            //StartCoroutine(TrailFade());
+            _line[count].enabled = true;
+            _line[count].SetPosition(0, firePos.transform.position);
+            _line[count].SetPosition(1, hit.point);
+            StartCoroutine(TrailFade());
             if (hit.collider.gameObject.TryGetComponent<HealthFunc>(out HealthFunc hf))
             {
                 hf.DamageCalcculate(_Edamage);
             }
         }
+
+        count++;
+        if(count == 3)
+        {
+            count = 0;
+        }
     }
 
-    //IEnumerator TrailFade()
-    //{
-
-    //}
+    IEnumerator TrailFade()
+    {
+        Material _mat = _line[count].material;
+        float fadeCount = 0.5f;
+        while(fadeCount > 0)
+        {
+            _mat.color = new Color(1, 0.9f, 0.7f, fadeCount);
+            fadeCount -= 0.01f;
+            yield return null;
+        }
+    }
 }
